5aafdadced63167c7aba6b455b22be73bf7d6de1
Liam Morgenthaler <liam.morgenthaler@icloud.com>
2022-01-27 23:35:50 +0100

    start of blockgeneration

--- Assets/Scripts/MapGeneration.cs
+++ Assets/Scripts/MapGeneration.cs
@@ -2,6 +2,8 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.ProBuilder;
+using UnityEngine.ProBuilder.MeshOperations;
+
 
 public class MapGeneration : MonoBehaviour
 {
@@ -10,21 +12,38 @@ public class MapGeneration : MonoBehaviour
     public Material material;
     void Start()
     {
+        
+        /* IList<Face> cubefaces = new List<Face>();
+        cubefaces = cube.faces; */
+
+        
+
+        ExtrudeElements.Extrude(cube, cube.faces, ExtrudeMethod.IndividualFaces, 1f);
+        Debug.Log("FaceCount: " + cube.faces.Count);
+        for(int i = 0; i < cube.faces.Count; i++){
+            Debug.Log("Face: "+cube.faces[i]);
+            cube.faces[i].Reverse();
+        }
+        
+
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+
+    void generateBlock()
+    {
         Vector3 test = new Vector3(1, 2, 3);
         Vector3 test2 = new Vector3(0, 0, 0);
         cube = ShapeGenerator.GenerateCube(PivotLocation.Center, test);
         cube.GetComponent<MeshRenderer>().material = material;
         cube.transform.position = test2;
-        cube.triangles = mesh.triangles.Reverse().ToArray();
-
-
-        // Update is called once per frame
-        void Update()
-        {
-
-
-
-        }
-
+        /* cube.triangles = mesh.triangles.Reverse().ToArray(); */
     }
 }
